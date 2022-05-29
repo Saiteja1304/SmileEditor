fcd9981d8bc4ed5cf1717a6d899d12824493a2f9
Saiteja <saiteja1304.st@gmail.com>
2022-05-29 09:18:54 +0000

    resolved flickering issue

--- SmileEditor.cpp
+++ SmileEditor.cpp
@@ -1,6 +1,37 @@
-#include <GLFW/glfw3.h>
-int main(){
+#include <Graphics/GL/Graphics.hpp>
+#include <Window/Window.hpp>
 
-    glfwInit();
-    return 0;
+// Set Singleton class's instance to 0
+Window *Window::WInstance = 0;
+Graphics *Graphics::GInstance = 0;
+
+int main() {
+  // Get window and graphics instances
+  Window *window = window->WGetInstance();
+  Graphics *graphics = graphics->GGetInstance();
+
+  // glfw init
+  window->WInitGLFW();
+  // create window
+  window->WCreateWindow();
+  // set created window to current context
+  window->WSetContext();
+
+  // load gl lib
+  graphics->GInit();
+  // set viewport size
+  graphics->GSetViewPort(0, 0, 800, 800);
+
+  // main loop
+  while (window->WIsRunning) {
+    // set BG color
+    graphics->GBackGroundColor(0.35, 0.45, 0.55, 1.0);
+    // update window
+    window->WUpdate();
+  }
+
+  // destroy window
+  window->WDestroyWindow();
+  // terminate glfw
+  window->WExitGLFW();
 }
