d0eb9c5a7c5dd81b496a6eb1723cb7cf7bd53de9
Saiteja <saiteja1304.st@gmail.com>
2022-05-31 17:39:34 +0530

    added script class

--- SmileEditor.cpp
+++ SmileEditor.cpp
@@ -1,14 +1,18 @@
+#include <Core/Input.hpp>
 #include <Graphics/GL/Graphics.hpp>
 #include <Window/Window.hpp>
 
+#include <iostream>
 // Set Singleton class's instance to 0
 Window *Window::WInstance = 0;
 Graphics *Graphics::GInstance = 0;
+Input *Input::IInstance = 0;
 
 int main() {
   // Get window and graphics instances
   Window *window = window->WGetInstance();
   Graphics *graphics = graphics->GGetInstance();
+  Input *input = input->IGetInstance();
 
   // glfw init
   window->WInitGLFW();
@@ -17,6 +21,9 @@ int main() {
   // set created window to current context
   window->WSetContext();
 
+  // Init Input
+  input->IInit();
+
   // load gl lib
   graphics->GInit();
   // set viewport size
@@ -24,10 +31,15 @@ int main() {
 
   // main loop
   while (window->WIsRunning) {
+    if(input->IGetKeyDown(INPUT_KEY_W)){
+      std::cout << "Key W pressed\n";
+    }
+    window->WPollEvents();
     // set BG color
     graphics->GBackGroundColor(0.35, 0.45, 0.55, 1.0);
     // update window
     window->WUpdate();
+    window->WSwapBuffers();
   }
 
   // destroy window
